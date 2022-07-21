c1b03c588db42aac54ad61d821ea4a8e2d766029
Arina-Putyatina <popowa.arina@yandex.ru>
2022-07-21 16:39:30 +0300

    Refactoring & MultiThreading

---
+++ src/main/java/Server.java
@@ -0,0 +1,110 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+    public static final int POOL_SIZE = 64;
+    public static final int PORT = 9999;
+    public static final int REQUEST_LINE_COUNT = 3;
+    public static List<String> validPaths = new ArrayList<>();
+
+    public static void start() {
+
+        ExecutorService service = Executors.newFixedThreadPool(POOL_SIZE);
+        fillValidPaths();
+
+        try (final var serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                try {
+                    final var socket = serverSocket.accept();
+                    service.submit(() -> connection(socket));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void fillValidPaths() {
+        File f = new File("public");
+        for (File s : f.listFiles()) {
+            if (s.isFile()) {
+                validPaths.add("/" + s.getName());
+            }
+        }
+    }
+    public static void connection(Socket socket) {
+        try (final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             final var out = new BufferedOutputStream(socket.getOutputStream());) {
+            // read only request line for simplicity
+            // must be in form GET /path HTTP/1.1
+            final var requestLine = in.readLine();
+            final var parts = requestLine.split(" ");
+
+            if (parts.length != REQUEST_LINE_COUNT) {
+                // just close socket
+                return;
+            }
+
+            final var path = parts[1];
+            if (!validPaths.contains(path)) {
+                sendNotFound(out);
+                return;
+            }
+
+            final var filePath = Path.of(".", "public", path);
+            final var mimeType = Files.probeContentType(filePath);
+
+            // special case for classic
+            if (path.equals("/classic.html")) {
+                final var template = Files.readString(filePath);
+                final var content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+
+                sendAnswer(out, mimeType, content.length);
+                out.write(content);
+                out.flush();
+                return;
+            }
+
+            final var length = Files.size(filePath);
+            sendAnswer(out, mimeType, length);
+            Files.copy(filePath, out);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void sendNotFound(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 404 Not Found\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+    }
+    public static void sendAnswer(BufferedOutputStream out, String mimeType, long length) throws IOException {
+        out.write((
+                "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+    }
+}
+
